3d1b54ac7b9932320f52212437d20ec9cb0d25b2
pacmadhushan <chathuramadhushanpa@gmail.com>
2022-12-20 16:27:21 +0530

    fix schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,10 +10,11 @@ CREATE TABLE IF NOT EXISTS `Project`(
     username VARCHAR(50) NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES `User`(username)
     );
-
+DROP TABLE IF EXISTS `Task`;
 CREATE TABLE IF NOT EXISTS `Task`(
     id INT AUTO_INCREMENT PRIMARY KEY,
     content VARCHAR(500) NOT NULL,
+    status ENUM('COMPLETED','NOT_COMPLETED') NOT NULL DEFAULT 'NOT_COMPLETED',
     project_id INT NOT NULL,
     CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES `Project`(id)
     );

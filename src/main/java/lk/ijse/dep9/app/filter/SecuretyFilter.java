ce0a921658b4967b2a5a8b23073d0d8880780f81
pacmadhushan <chathuramadhushanpa@gmail.com>
2022-12-21 18:45:03 +0530

    add Security filter

---
+++ src/main/java/lk/ijse/dep9/app/filter/SecuretyFilter.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep9.app.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SecuretyFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Incoming Request");
+
+    }
+}

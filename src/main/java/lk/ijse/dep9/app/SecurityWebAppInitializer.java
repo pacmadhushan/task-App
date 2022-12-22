426d93642c1b2c394787ede42d59b63eea7bf520
pacmadhushan <chathuramadhushanpa@gmail.com>
2022-12-22 20:25:18 +0530

    securety spring

---
+++ src/main/java/lk/ijse/dep9/app/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep9.app;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
